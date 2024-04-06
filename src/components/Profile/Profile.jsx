4a82e5215d66f5b2f3dc449b3f8d1f36fa9a16c6
sofia tselen <sofiatselenx@gmail.com>
2024-04-06 21:35:15 +0300

    1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,32 @@
+import css from "./Profile.module.css";
+export default function Profile({ data: { username, tag, location, avatar, stats: {
+    followers, views, likes } } }) {
+    return   <>
+        <div className={css.container}>
+  <div>
+    <img className={css.avatar} 
+      src={avatar}
+      alt={username}
+    />
+    <p className={css.username}>{username}</p>
+    <p className={css.tag}>@{tag}</p>
+    <p className={css.location}>{location}</p>
+  </div>
+
+  <ul className={css.list}>
+    <li className={css.item}>
+      <span className={css.span}>Followers</span>
+      <span className={css.span}>{followers}</span>
+    </li>
+    <li className={css.item}>
+      <span className={css.span}>Views</span>
+      <span className={css.span}>{views}</span>
+    </li>
+    <li className={css.item}>
+      <span className={css.span}>Likes</span>
+      <span className={css.span}>{likes}</span>
+    </li>
+  </ul>
+</div>
+    </>
+}
